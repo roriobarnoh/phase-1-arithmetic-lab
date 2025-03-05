7ffbbdf63908cc071d0412b6d0a0c7a41e708183
roriobarnoh <roriokiptoo2016@gmail.com>
2025-03-05 11:31:36 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1* num2;
+console.log(multiply);
+
+const random =Math.floor (Math.random() *10) + 5 ;
+console.log(random);
+
+const num3 = 9;
+const num4 = 5;
+const mod = num3%num4;
+
+const max = Math.max(20,19,14,12,17);
+console.log(max);
